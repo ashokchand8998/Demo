06dc4969f7debb2ec6d7051909e47c1b0680ee55
“Ashok <ashok.t@sankeysolutions.com>
2020-12-12 15:16:56 +0530

    added code to use databse on deployment

--- api/app/models/index.js
+++ api/app/models/index.js
@@ -1,22 +1,39 @@
 //initailizing sequelize
-const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+let sequelize = null;
 
-/*Creating connection to a database
+
+/*
+*Creating connection to a database
 *Sequelize(database_name, user, password, {.....})
 */
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host : dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    //to prevent sequelize log out executed queries in console
-    logging:false,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
-});
+if(process.env.DATABASE_URL) {
+    // the application is running on Heroku ......
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        port: match[4],
+        logging: false
+    })
+} else {
+    //application is running on local machine
+    const dbConfig = require("../config/db.config.js");
+
+    sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+        host : dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        //to prevent sequelize log out executed queries in console
+        logging:false,
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
+        }
+    });
+
+}
+
 
 const db = {};
 db.Sequelize = Sequelize;
